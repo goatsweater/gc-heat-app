41d5eaf3a70397f0246b728cf35cbdb11dba968c
Reg Maltais <reginald.maltais@gmail.com>
2015-09-16 18:16:02 -0400

    save and reload user settings

--- src/lib/heat.js
+++ src/lib/heat.js
@@ -15,3 +15,28 @@ var HEAT = {
     },
   }
 }
+
+// Settings page storage and loading
+$(document).ready(function () {
+  if (window.location.href.indexOf("settings") > -1)
+  {
+    // load values from local storage
+    var userage = parseInt(localStorage['settings.userage']);
+    $('#userage').val(userage);
+    var tracklvl = localStorage['settings.tracklvl'];
+    if (tracklvl == 0) {
+      $("#tracklvl1").prop('checked', true);
+    } else if (tracklvl == 1) {
+      $("#tracklvl2").prop('checked', true);
+    }
+
+    // save on value change
+    $('#userage').blur(function() {
+      localStorage['settings.userage'] = $('#userage').val();
+    });
+    $("input[type='radio'][name='tracklvl']").change(function() {
+      localStorage['settings.tracklvl'] = $("input[type='radio'][name='tracklvl']:checked").val();
+      $(this).blur();
+    });
+  }
+});
