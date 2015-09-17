67f18be6e71b4adf1f625826cfc0e7c029a9d974
Reg Maltais <reginald.maltais@gmail.com>
2015-09-17 14:45:54 -0400

    skip request to fill out common info

--- src/lib/heat.js
+++ src/lib/heat.js
@@ -10,11 +10,90 @@ var HEAT = {
       step = HEAT.survey.currentStep;
       $('#step' + step).addClass('hidden');
       step += 1;
+      // Skip step 2 if you've already filled it out
+      if (step == 2 && HEAT.survey.userInfoDefined()) {
+        step += 1;
+      }
       $('#step' + step).removeClass('hidden');
       HEAT.survey.currentStep = step;
     },
+    userInfoDefined: function () {
+      var usersex = localStorage["settings.usersex"];
+      console.log(usersex);
+      var userage = localStorage["settings.userage"];
+      console.log(userage);
+      var vaccinated = localStorage["survey.vaccinated"];
+      console.log(vaccinated);
+
+      if (userage && usersex && vaccinated) {
+        return true;
+      } else {
+        return false;
+      },
+      done: function() {
+        HEAT.survey.currentStep = 1;
+      }
+    }
   }
 }
+
+// Survey questions need to know the current location of the user
+survey_position = function (loc) {
+  console.log("User position: " + loc.coords.latitude + ", " + loc.coords.longitude);
+};
+
+no_position = function (err) {
+  if (err.code == 1) {
+    console.log("User said no to sharing GPS");
+  } else if (err.code == 2) {
+    console.log("Position request timed out");
+  }
+};
+
+$(document).ready(function () {
+  if (window.location.href.indexOf("survey") > -1)
+  {
+    navigator.geolocation.getCurrentPosition(survey_position, no_position, {enableHighAccuracy: false});
+    console.log("Asked for user position");
+
+    // Try to fill the birth date if known
+    var userage = parseInt(localStorage['settings.userage']);
+    if (userage) {
+      $('#birth').val(userage);
+    }
+    // Try to fill the sex if known
+    var usersex = localStorage['settings.usersex'];
+    if (usersex == 0) {
+      $('#usersex1').prop('checked', true);
+    } else if (usersex == 1) {
+      $('#usersex2').prop('checked', true);
+    }
+    // Try to fill the vaccinated if known
+    var vaccinated = localStorage['survey.vaccinated'];
+    if (vaccinated == 1) {
+      $('#vaccinated').prop('checked', true);
+    }
+
+    // save on value change
+    $('#birth').blur(function() {
+      localStorage['settings.userage'] = $('#birth').val();
+      console.log("birthday stored");
+    });
+    $("input[type='radio'][name='sex']").change(function() {
+      localStorage['settings.usersex'] = $("input[type='radio'][name='sex']:checked").val();
+      $(this).blur();
+      console.log("sex stored");
+    });
+    $('#vaccinated').change(function () {
+      console.log('vaccine triggered');
+      if ($("input[name='vaccinated']:checked").length > 0)
+      {
+        localStorage['survey.vaccinated'] = 1;
+        console.log('vaccinated stored');
+      }
+    });
+  }
+});
 
 // Settings page storage and loading
 $(document).ready(function () {
